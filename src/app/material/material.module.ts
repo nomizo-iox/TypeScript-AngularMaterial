5d02637bf7614fd836a4ae538490bb4a1c3a8624
nomizo-iox <nomizo.iox@gmail.com>
2020-03-28 11:28:06 -0500

    added some icons.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatButtonToggleModule} from '@angular/material';
+import {MatButtonModule, MatButtonToggleModule, MatIconModule} from '@angular/material';
 
 
-const MaterialComponents = [MatButtonModule, MatButtonToggleModule];
+const MaterialComponents = [MatButtonModule, MatButtonToggleModule, MatIconModule];
 
 @NgModule({
   imports: [
